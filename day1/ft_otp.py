02ebd6a84c1ab179327b325d73597434244ba90e
Maxime Winter-knecht <maxx@Mac.local>
2023-05-16 22:38:27 +0200

    lol

--- day1/ft_otp.py
+++ day1/ft_otp.py
@@ -1,48 +1,73 @@
+import hashlib
+import hmac
 from cryptography.fernet import Fernet
-import pickle
-import pyotp
 import sys
+import os
+import re
+import base64
+import binascii
 
-#------------------- Starting --------------------------#
+def parsing(nb_args):
+   counter = 1
+   generate = False
+   encrypt_key = None
+   hexa_key = None
+   save = False
+   generate_key = False
 
-nb_args = len(sys.argv)
+   if (nb_args == 1):
+      print("Too few args")
 
-print(nb_args)
+   while counter < nb_args:
+      if sys.argv[counter] == "-k":
+         generate = True
+         if counter + 1 is not nb_args - 1:
+            print("please enter only an encrypted key after -k")
+            sys.exit(1)
+         else:
+            counter += 1
+            if os.path.exists(sys.argv[counter]):
+               with open(sys.argv[counter + 1], 'r') as fichier:
+                  encrypt_key = fichier.read()
+            else:
+               encrypt_key = sys.argv[counter]
+           
+      elif sys.argv[counter] == "-g":
+         save = True
+         if counter + 1 == nb_args:
+            generate_key = True
+         else:
+            counter += 1
+            if os.path.exists(sys.argv[counter]):
+               with open(sys.argv[counter], 'r') as fichier:
+                  hexa_key = fichier.read()
+            else:
+                  hexa_key = sys.argv[counter]
+         motif = r'^[0-9a-fA-F]{64}$'
+         if hexa_key is not None and not re.match(motif, hexa_key):
+            print ("The key must be a key of 64 hexadecimal characters")
+            sys.exit(1)
+
+      else:
+         print("Unavailable argument detected")
+         sys.exit(1)
+      counter += 1
+   return (generate,encrypt_key,hexa_key,save,generate_key)
 
 
-key = Fernet.generate_key()  
-
-with open('starting.key', 'wb') as filekey:
-   filekey.write(key)
-
-#----------------------- B --------------------------#
 
 
-B = Fernet(key)    
-
-
-with open('b.key', 'wb') as filekey:
-   pickled_b = pickle.dumps(B)       #ne sert a rien, juste pour rendre le bail visuel
-   filekey.write(pickled_b)
-
-
-#----------------------- A( = token) --------------------------#
-
-token = B.encrypt("my deep dark secret".encode()) #encrypter le message avec B
+# def save_encryted_key(hexa_key64):
+#    encrypted_key = Fernet(hexa_key64)
+#    with open('./ft_otp.key', 'wb') as fichier:
+#          fichier.write(encrypt_key)
+   
 
 
 
-with open('a.key', 'wb') as filekey:  #ecrire le message crypter A
-   filekey.write(token)
+generate,encrypt_key,hexa_key,save,generate_key= parsing(len(sys.argv))
 
-
-#decription du token(A) grace a B
-
-decrypted = B.decrypt(token)
-
-print(decrypted)
-print(decrypted.decode())
-
-
-
+if save:
+   if generate_key:
+      encrypt_key = Fernet.generate_key()
 
